9935a24af3f249c4178da9b32c2637d83f029e6d
Carl Bernardo <carlbernrdo@gmail.com>
2016-04-26 08:35:58 -0700

    [fix] fixes hardcoded port in server

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../', 'index.html'))
 });
 
-app.listen(port, () => (
-  console.log('listening on port 3000!')
-));
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
