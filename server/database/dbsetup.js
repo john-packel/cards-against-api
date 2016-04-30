3d6c414eacefafc991920c00ac7a10996b3d71ed
Carl Bernardo <carlbernrdo@gmail.com>
2016-04-30 11:12:24 -0700

    [refactor] refactors dbsetup to use const, adds success logs

--- server/database/dbsetup.js
+++ server/database/dbsetup.js
@@ -3,9 +3,12 @@ import co from 'co';
 
 export default co(function*() {
   // Connection URL
-  var url = 'mongodb://localhost:27017/cardsAPI';
+  const url = 'mongodb://localhost:27017/cardsAPI';
+  console.log('Connection: Success');
+
   // Use connect method to connect to the Server
-  var db = yield MongoClient.connect(url);
+  const db = yield MongoClient.connect(url);
+
   // Close the connection
   db.close();
 }).catch(function(err) {
