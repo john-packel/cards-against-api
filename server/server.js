4d32d31517c69335154d7613ed4d9970abce68c0
Carl Bernardo <carlbernrdo@gmail.com>
2016-04-30 12:39:08 -0700

    [setup] sets up basic api router

--- server/server.js
+++ server/server.js
@@ -9,9 +9,14 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//-----------------------------------------------------------------------------------
+import apiRouter from './routes/apiRouter';
+// ROUTES
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../', 'index.html'))
 });
+
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
   console.log('listening on port', port);
