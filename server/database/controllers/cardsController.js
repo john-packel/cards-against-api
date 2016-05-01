8f405bc90f0223c7a59b7697ebd31ac7f98c7266
Carl Bernardo <carlbernrdo@gmail.com>
2016-05-01 15:17:53 -0700

    [refactor] refactors cardsController and route

--- server/database/controllers/cardsController.js
+++ server/database/controllers/cardsController.js
@@ -1,22 +1,23 @@
 import { MongoClient } from 'mongodb';
 
-let cardsController = function*(req, res) {
-  // console.log('REQ:', req);
+const cardsController = {
+  * fetchCards(req, res) {
+    // Connection URL
+    const url = 'mongodb://localhost:27017/cardsAPI';
 
-  // Connection URL
-  const url = 'mongodb://localhost:27017/cardsAPI';
+    // Use connect method to connect to the Server
+    const db = yield MongoClient.connect(url);
 
-  // Use connect method to connect to the Server
-  const db = yield MongoClient.connect(url);
+    // Retrieve cards
+    let col = db.collection('decks');
+    let cards = yield col.find().toArray();
 
-  // Retrieve cards
-  let col = db.collection('decks');
-  console.log('col:', col);
-  let cards = yield col.find().toArray();
+    db.close();
 
-  db.close();
-
-  return cards;
+    return cards;
+  }
 };
+
+
 
 export default cardsController;
--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,19 +1,18 @@
 import { Router } from 'express';
 import co from 'co';
 
-// Controller
+// Controllers
 import cardsController from './../database/controllers/cardsController';
 
 const apiRouter = Router();
 
 apiRouter.get('/', (req, res) => {
-  co(cardsController(req, res)).then(val => {
-    console.log('in route', val.length)
+  co(cardsController.fetchCards(req, res)).then(val => {
+    console.log('Sending:', val);
+    res.send(val);
   }).catch((err) => {
     console.log(err.stack);
   });
-
-  res.send('Hello from apiRouter');
 });
 
 export default apiRouter;
