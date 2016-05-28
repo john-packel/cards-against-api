a55f6b086ed0052b94695ca6310cf1280af6749a
msarnicki <melindabernrdo@gmail.com>
2016-05-28 08:20:10 -0700

    [progress] creates auth route and sends to server

---
+++ server/config/passport.js
@@ -0,0 +1,8 @@
+
+var auth = {};
+
+auth.handleAuth = (req, res) => {
+  console.log("handling auth");
+}
+
+export default auth;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 import db from './database/dbsetup';
 
 const app = express();
@@ -8,13 +9,17 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(passport.initialize());
 
 //-----------------------------------------------------------------------------------
 import apiRouter from './routes/apiRouter';
+import auth from './config/passport.js'
 // ROUTES
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../', 'index.html'))
 });
+
+app.post('/signup', auth.handleAuth);
 
 app.use('/api', apiRouter);
 
